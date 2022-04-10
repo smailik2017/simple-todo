5d015e5c7bea3baf4739f2b7f455e37c16421fda
mvfedorovskiy <mvfedorovskiy@gmail.com>
2022-04-10 19:38:33 +0700

    simple-todo template

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'settings', to: 'settings#index', as: 'settings_index'
+  get 'help', to: 'help#index', as: 'help_index'
+  get 'about', to: 'about#index', as: 'about_index'
+
+  root 'tasks#index'
+end

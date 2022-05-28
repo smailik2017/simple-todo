dfc5b9ba4deb723b7db3978c364de0cf80966d17
mvfedorovskiy <mvfedorovskiy@gmail.com>
2022-05-28 21:57:24 +0700

    views_helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_date
+    Date.current.to_s
+  end
+
+  def list_tag(values)
+    result = "<ul>"
+    values.each do |value|
+      result << "<li>#{value}</li>"
+    end
+    sanitize result << "</ul>"
+  end
 end

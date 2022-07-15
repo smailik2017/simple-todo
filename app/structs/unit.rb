2aa06fb61598096c84a02107759cae4c16773f2a
mvfedorovskiy <mvfedorovskiy@gmail.com>
2022-07-15 10:59:26 +0700

    Arc_rails_proj_1

--- app/structs/unit.rb
+++ app/structs/unit.rb
@@ -8,17 +8,23 @@ class Unit < Dry::Struct
     attribute :url, Types::String
   end
 
+  # Цена позиции в рублях
   def in_rub
-    return price if currency == 'RUB'
     rate_currency(currency) * price
   end
 
   private
 
+  # Курс валюты к рублю. 
+  ## Если данные неправильные, возвращаем 0
+  ## Если текущая валюты RUB возвращаем 1
   def rate_currency(valute)
+    return 1 if valute == 'RUB'
     src = 'https://www.cbr-xml-daily.ru/daily_json.js'
     response = HTTParty.get(src)
-    CurrencyCbr.new(JSON.parse(response.body)['Valute'].select { |v| v == valute }[valute].map { |k, v| [k.to_sym, v] }.to_h).Value
+    cbr = JSON.parse(response.body)['Valute'].select { |v| v == valute }[valute].map { |k, v| [k.to_sym, v] }.to_h
+    result = Contracts::CurrencyCbr.call(cbr)
+    result.success? ? result[:Value] : 0
   end
 
 end
